5067c1e11e07be8e44553872a3e5f762f4b45d69
Johan roug <johanroug@gmail.com>
2012-11-08 11:09:36 +0100

    input focus, cleanup

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,6 +13,7 @@
 	//Window load
 	$(window).load(function (){
     	cleargrid();
+    	inputFocus();
 	});	
 
 	//****************************************************************************************************************
@@ -41,6 +42,22 @@
 		        }
 		    });
 		}); 		
+	}
+
+	//Input focus
+	function inputFocus(){
+		 $('.input').on('focus', function() {
+	        // On first focus, check to see if we have the default text saved
+	        // If not, save current value to data()
+	        if (!$(this).data('defaultText')) $(this).data('defaultText', $(this).val());
+
+	        // check to see if the input currently equals the default before clearing it
+	        if ($(this).val()==$(this).data('defaultText')) $(this).val('');
+	    });
+	    $('.input').on('blur', function() {
+	        // on blur, if there is no value, set the defaultText
+	        if ($(this).val()=='') $(this).val($(this).data('defaultText')); 
+	    });
 	}
 	
 	
